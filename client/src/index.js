d84f2bf71270cdbc2bf3cc28b8b597eae1a4d7b1
Ernie Rimer <ernest.rimer@icloud.com>
2020-07-11 12:33:05 -0600

    routes

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import 'semantic-ui-css/semantic.min.css';
 import { BrowserRouter } from 'react-router-dom';
 import { initMiddleware } from 'devise-axios';
-import { AuthProvider } from './providers/AuthProvider';
+import AuthProvider from './providers/AuthProvider';
 
 initMiddleware()
 
